c9d498da91fddcbe247b974806395e55db089164
Brandon Nguyen <bnguyen9469@yahoo.com>
2015-12-21 23:53:41 -0800

    Final competition code

--- Macros.h
+++ Macros.h
@@ -1,3 +1,17 @@
 #define min(x,y) ((x > y) ? (y) : (x))
 #define max(x,y) ((x > y) ? (x) : (y))
 #define safeSet(power) (max(-MAX_POWER, min(MAX_POWER, power)))
+
+#define drive(l,r) motor[lDrive] = l; motor[rDrive] = r
+
+#define liftGate() motor[gate] = GATE_SPEED
+#define dropGate() motor[gate] = -GATE_SPEED
+#define stopGate() motor[gate] = 0
+
+#define startRight() motor[rTopShooter] = SHOOT_DEFAULT_SPEED; motor[rBottomShooter] = SHOOT_DEFAULT_SPEED
+#define stopRight() motor[rTopShooter] = 0;	motor[rBottomShooter] = 0
+#define startLeft() motor[lTopShooter] = SHOOT_DEFAULT_SPEED; motor[lBottomShooter] = SHOOT_DEFAULT_SPEED
+#define stopLeft() motor[lTopShooter] = 0;	motor[lBottomShooter] = 0
+
+#define startBoth() startLeft(); startRight()
+#define stopBoth() stopLeft(); stopRight()
--- Robot.h
+++ Robot.h
@@ -1,24 +1,15 @@
 /* Shooter Definitions */
 
 //Values
-#define SHOOT_DEFAULT_SPEED 120
-#define KONSTANT 5
+#define SHOOT_DEFAULT_SPEED 127
 
 //Joystick Buttons
 //auto = alwaysOnEnc; manual = whenButtonPressedOnly
 #define autoOn vexRT[Btn5U] //turn on auto
 #define autoOff vexRT[Btn5D] //turn off auto
 #define startR vexRT[Btn8R] //right manually
-#define startL vexRT[Btn7L] //left manually
-#define startBM vexRT[Btn6U] //both manually
-
-//Encoders
-#define shRT nMotorEncoder[rTopShooter] //master
-#define shLT nMotorEncoder[lTopShooter]
-#define shRB nMotorEncoder[rBottomShooter]
-#define shLB nMotorEncoder[lBottomShooter]
-
-#define err(enc) shRT - enc
+#define startL vexRT[Btn8L] //left manually
+#define startBM vexRT[Btn8U] //both manually
 
 
 /* Drive Definitions */
@@ -28,11 +19,11 @@
 #define joyRDr vexRT[Ch2]
 
 
-/* Intake Definitions */
+/* Gate Definitions */
 
 //Joystick
-#define joyIntakeI vexRT[Btn8L]
-#define joyIntakeO vexRT[Btn8R]
+#define joyGateUp vexRT[Btn6U]
+#define joyGateDown vexRT[Btn6D]
 
 //Values
-#define INTAKE_SPEED 110
+#define GATE_SPEED 110
